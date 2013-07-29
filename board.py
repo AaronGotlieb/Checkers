0d6df5bc9ad8e485ec541df9f2ca569ed0ed7ea2
zork3 <agotlieb16@gmail.com>
2013-07-29 13:18:16 -0400

    board creation

--- board.py
+++ board.py
@@ -1,3 +1,18 @@
 '''
 Aaron Gotlieb
 '''
+
+class makeBoard(board):
+	board = [[0 for x in range(8)] for x in range(8)] 
+	for x in range (0, 8):
+		for y in range (0, 8):
+			if (y % 2 == 1 and x % 2 == 1):
+				board[x][y] = 1
+			if (y % 2 == 0 and x % 2 == 0):
+				board[x][y] = 1
+	
+
+class main():
+	board
+	board = makeBoard()
+
